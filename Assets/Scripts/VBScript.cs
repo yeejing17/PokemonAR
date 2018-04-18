f599e17a97976a81d86326ecda657a2dbcc51c3b
yeejing17 <yeejing17@gmal.com>
2018-04-19 05:04:40 +0800

    First commit

---
+++ Assets/Scripts/VBScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class VBScript : MonoBehaviour, IVirtualButtonEventHandler {
+
+    private GameObject vbButtonObject;
+    private GameObject pokemon;
+    private Animator animator;
+    private AudioSource audio;
+
+    // Use this for initialization
+    void Start () {
+        vbButtonObject = GameObject.Find("actionButton");
+        pokemon = GameObject.Find("Pokemon");
+        animator = pokemon.GetComponent<Animator>();
+        audio = pokemon.GetComponent<AudioSource>();
+        vbButtonObject.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+	}
+	
+    public void OnButtonPressed(VirtualButtonAbstractBehaviour vb)
+    {
+        Debug.Log("button pressed");
+        animator.Play("Anim001", -1, 0F);
+        audio.Play();
+    }
+
+    public void OnButtonReleased(VirtualButtonAbstractBehaviour vb)
+    {
+        Debug.Log("button released");
+    }
+
+}
